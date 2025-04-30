f999d40325298852a993929dd2fe6731b4e6e02c
mihai <mihai.sanfran@gmail.com>
2025-04-30 12:24:45 -0700

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+//import Header from "./components/header.component";
+import Home from "./routes/home/home.component";
+// import Sidebar from "./components/sidebar/sidebar.jsx";
+// import Content from "./components/content/content.jsx";
 
 function App() {
-  return <div className="App">TradeTrackr</div>;
+  return (
+    <div>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+
+      {/* <Sidebar />
+      <Content /> */}
+    </div>
+  );
 }
 
 export default App;
